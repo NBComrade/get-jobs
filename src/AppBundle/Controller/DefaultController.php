9978343ad8ba72a0a005d574d58ef8fb8d77b1ac
NBComrade <bogdan.nediak@web100.com.ua>
2018-03-06 16:51:39 +0200

    Initialized Webapck-encore for project

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,25 +3,35 @@
 namespace AppBundle\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="app.homepage")
+     * @Method({"GET"})
      */
     public function indexAction(Request $request) : Response
     {
         $sender = $this->get('app.sender');
-		$request = new \GuzzleHttp\Psr7\Request('GET', 'https://github.com/');
+		$request = new \GuzzleHttp\Psr7\Request('GET', 'https://work.ua/');
         $gitHub = $sender->sendRequest($request, function (ResponseInterface $response) {
             return 'I completed! ' . $response->getBody();
         });
 
         return new Response($gitHub);
+    }
+
+    /**
+     * @Route("/parse", name="app.parse-data")
+     * @Method({"POST"})
+     */
+    public function parseAction(Request $request) : Response
+    {
+        dump($request);
     }
 }
